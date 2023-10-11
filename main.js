f735a1f5ea9689db38ed04875977a9f8714dc39a
ThiagoJM <thiagojm.23@hotmail.com>
2023-10-11 00:47:48 -0300

    Parte visual da página no modo dark pronta, apenas faltando pequenas coisas em JS para encerrar

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
-const teste = document.querySelectorAll(".todos__lista p button");
-
-teste.forEach((botao) => {
-  botao.addEventListener("click", () => {
-    if (botao.classList.contains("ativado")) {
-      botao.classList.remove("ativado");
-    } else {
-      botao.classList.add("ativado");
+const novoTodo = document.querySelector(".cabecalho__novo-todo input");
+novoTodo.addEventListener("keydown", (evento) => {
+  if (evento.keyCode === 13) {
+    const listaNode = document.querySelectorAll(".todos__lista p");
+    const lista = Array.from(listaNode).slice(0, -1);
+    console.log(lista);
+    for (const elemento of lista) {
+      console.log(elemento.classList.contains("ultimo"));
+      if (elemento.outerText === "") {
+        console.log("indo");
+        elemento.insertAdjacentHTML("beforeend", novoTodo.value);
+        novoTodo.value = "";
+        break;
+      } else if (elemento.classList.contains("ultimo")) {
+        console.log("batata doce");
+        const divLinha = document.createElement("div");
+        divLinha.innerHTML = `<p>
+        <button><img /></button>
+        </p>`;
+        const paiLista = document.querySelector(".todos__lista div");
+        paiLista.appendChild(divLinha);
+        novoTodo.value = "";
+      }
     }
-  });
+    const checks = document.querySelectorAll(".todos__lista p button");
+    const imagemCheck = document.querySelectorAll(".todos__lista p button img");
+    checks.forEach((botao, index) => {
+      const copy = botao;
+      if (botao != false && botao != true) {
+        botao.addEventListener("click", () => {
+          if (botao === false) {
+            botao = copy;
+            botao.classList.remove("ativado");
+            botao.removeAttribute("style");
+            imagemCheck[index].src = "";
+            botao = true;
+          } else {
+            botao = copy;
+            botao.classList.add("ativado");
+            imagemCheck[index].src = "images/icon-check.svg";
+            botao = false;
+          }
+        });
+      }
+    });
+  }
 });
+/*utilizar o for of para percorrer o array lista e fazer com que ele adicione
+o valor do input no campo abaixo que está vazio, no caso adicionar nos <p>*/
